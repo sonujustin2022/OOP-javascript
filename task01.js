9585051601eeb1125f2dd2b98747425284a15951
sonujustin2022 <sonujustin6282@gmail.com>
2026-04-01 13:09:11 +0530

    task01 finsihed

---
+++ task01.js
@@ -0,0 +1,19 @@
+class student{
+    name = "bobin"
+    age = 34
+    maths = 56
+    chems = 56
+    attendence = "present"
+
+    //methods
+    print(){
+        console.log(this.name,this.age);
+
+
+    }
+}
+
+let obj01 = new student();
+
+obj01.print();
+
