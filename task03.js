5463b04b5e36751c2d5a53465c7d7b2851278205
sonujustin2022 <sonujustin6282@gmail.com>
2026-04-02 19:11:36 +0530

    ---

---
+++ task03.js
@@ -0,0 +1,47 @@
+class Bank {
+  // property
+  accountDetails = {
+    1000: {
+      accno: 1000,
+      userName: "userOne",
+      password: "password1",
+      balance: 5000,
+    },
+    1001: {
+      accno: 1001,
+      userName: "userTwo",
+      password: "password2",
+      balance: 7000,
+    },
+    1002: {
+      accno: 1002,
+      userName: "userThree",
+      password: "password3",
+      balance: 8000,
+    },
+    1003: {
+      accno: 1003,
+      userName: "userFour",
+      password: "password4",
+      balance: 6000,
+    },
+    1004: {
+      accno: 1004,
+      userName: "userFive",
+      password: "password5",
+      balance: 9000,
+    },
+  };
+
+  //   methods
+  // Validate account no : (check account no is there)
+
+
+  // authenticate account only for those with valid account no (acccno, username & password)
+
+
+
+  // balance enquiry only for authenticated ones (accno, username & password)
+
+
+}
