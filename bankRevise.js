0abe5385a2214ab5727000aad2c7ee34e6552216
sonujustin2022 <sonujustin6282@gmail.com>
2026-04-04 11:41:33 +0530

    deposit and withdraw finished

--- bankRevise.js
+++ bankRevise.js
@@ -27,12 +27,73 @@ class Bank {
 
   // 1. validateAccount(accNo) → check if account exists
 
+toValidate(accNo){
+    return Object.hasOwn(this.accounts,accNo);
+
+}
+
+
   // 2. deposit(accNo, amount) → increase balance
 
+
+toDeposit(accNo,amount){
+
+    if(this.toValidate(accNo)){
+        if(isNaN(amount) || amount<=0){
+            return "not valid amount"
+        }
+
+        let account = this.accounts[accNo];
+
+        account.balance += amount;
+        return account.balance;
+
+    }else {
+        return "account not found"
+    }
+
+}  
+
   // 3. withdraw(accNo, amount) → decrease balance (only if sufficient balance)
+
+
+  toWithdraw(accNo,amount){
+
+    if(!this.toValidate(accNo)){
+        return "account not found"
+    }
+
+    if(isNaN(amount)|| amount<=0){
+        return "invalid number"
+    }
+
+    let account = this.accounts[accNo];
+
+    if(amount<=account.balance){
+        account['balance'] -= amount;
+        return account.balance;
+
+    }else{
+        return "insufficent amount"
+    }
+
+  }
+
 
   // 4. checkBalance(accNo) → display balance
 
   // 5. fundTransfer(fromAcc, toAcc, amount)
   //    → transfer money if both accounts exist and balance is sufficient
 }
+
+let bankOb = new Bank();
+
+// let validate = bankOb.toValidate(1005);
+
+// validate?console.log("validation succesfull"):console.log("validation not successfull")
+
+// let toDeposit = bankOb.toDeposit(1004,3000);
+// console.log(toDeposit);
+
+let toWithDraw = bankOb.toWithdraw(1004,5000);
+console.log(toWithDraw);
